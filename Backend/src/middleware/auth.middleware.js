c64cd174f2ac6ec235639fd4742e3678e1fc173d
Harshal Varade <developer.ever@gmail.com>
2026-03-24 02:19:39 +0530

    Bug finding and fixing

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -37,7 +37,7 @@ async function authUserMiddleware(req, res, next) {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await userModel.findById(decoded.id);
-    req.user = decoded;
+    req.user = user;
     next();
   } catch (err) {
     return res.status(401).json({
